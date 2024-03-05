4e226885163b16aca095fef29ac437e7a236c8c3
Yevhenii <zhenyabroddd@gmail.com>
2024-03-05 13:34:22 +0100

    [Dev] Add base controller

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,27 @@
-import express, {Express} from 'express';
-import { userRouter } from './users/users';
-import {Server} from 'http';
+import express, { Express } from "express";
+import { userRouter } from "./users/users";
+import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
 
 export class App {
-    app: Express;
-    server: Server;
-    port: number;
+  app: Express;
+  server: Server;
+  port: number;
+  logger: LoggerService;
 
-    constructor() {
-        this.app = express();
-        this.port = 8000;
-    }
+  constructor(logger: LoggerService) {
+    this.app = express();
+    this.port = 8000;
+    this.logger = logger;
+  }
 
-    useRoutes() {
-        this.app.use('/users', userRouter);
-    }
+  useRoutes() {
+    this.app.use("/users", userRouter);
+  }
 
-    public async init() {
-        this.useRoutes();
-        this.server = this.app.listen(this.port);
-        console.log(`Server started at http://localhost:${this.port}`);
-    }
+  public async init() {
+    this.useRoutes();
+    this.server = this.app.listen(this.port);
+    this.logger.log(`Server started at http://localhost:${this.port}`);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { App } from "./app";
+import { LoggerService } from "./logger/logger.service";
 
 async function bootstrap() {
-    const app = new App();
-    await app.init();
+  const app = new App(new LoggerService());
+  await app.init();
 }
 
 bootstrap();
